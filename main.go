1da73ebe589afa1c38a868b9fd58a6b0b0ecddf3
evenardo <evenardo@gmail.com>
2018-11-12 09:18:54 +0800

    uart

--- main.go
+++ main.go
@@ -12,48 +12,63 @@ func main() {
 	defer rpio.Close()
 
 	go read()
-	go send()
 
-	time.Sleep(time.Second * 15)
+	for {
+		time.Sleep(time.Second * 15)
+	}
 }
 
 func read() {
+
 	u := uart.Uart{
 		Rx: rpio.Pin(4),
 		Tx: rpio.Pin(5),
 	}
 
-	var res = u.Read()
-	fmt.Println(res)
-}
-
-func send() {
-	cnt := 0
-	p3 := rpio.Pin(17)
-
-	p3.Output()
-	p3.High()
-	time.Sleep(time.Second * 3)
-
-	//
-	var number uint8
-	number = 53
-
-	ticker := time.NewTicker(time.Second)
-	for range ticker.C {
-		if cnt == 0 {
-			cnt++
-			p3.Low()
-		} else {
-			cnt++
-			var bit8 uint8
-			bit8 = number << 7
-			p3.Write(rpio.State(bit8 >> 7))
-			if cnt == 9 {
-				break
-			}
-			number = number >> 1
-		}
-
+	for {
+		u32 := u.Read32()
+		fmt.Println(u32)
 	}
+
+	//cnt := 0
+	//for i := 0; i < 500; i++ {
+	//	var res = u.Read()
+	//	fmt.Println(i, res)
+	//if res != 97 {
+	//	cnt++
+	//	break
+	//}
+	//}
 }
+
+//
+//func send() {
+//	cnt := 0
+//	p3 := rpio.Pin(17)
+//
+//	p3.Output()
+//	p3.High()
+//	time.Sleep(time.Second * 3)
+//
+//	//
+//	var number uint8
+//	number = 53
+//
+//	ticker := time.NewTicker(time.Second / 900)
+//	for range ticker.C {
+//		if cnt == 0 {
+//			cnt++
+//			p3.Low()
+//		} else {
+//			cnt++
+//			var bit8 uint8
+//			bit8 = number << 7
+//			p3.Write(rpio.State(bit8 >> 7))
+//			if cnt == 9 {
+//				break
+//			}
+//			number = number >> 1
+//		}
+//
+//	}
+//}
--- uart/uart.go
+++ uart/uart.go
@@ -12,8 +12,46 @@ type Uart struct {
 }
 
 var (
-	ticker = time.NewTicker(time.Second)
+	ticker = time.NewTicker(time.Second / 900)
 )
+
+func (uart Uart) Read32() (buf uint32) {
+	uart.Rx.Input()
+	var count = 0
+	for range ticker.C {
+
+		if count == 0 || count == 10 || count == 20 || count == 30 {
+			if uart.Rx.Read() == rpio.Low {
+				count++
+				continue
+			}
+		}
+
+		if count == 9 || count == 19 || count == 29 {
+			count++
+		}
+		if (count > 0 && count < 9) ||
+			(count > 10 && count < 19) ||
+			(count > 20 && count < 29) ||
+			(count > 30 && count < 39) {
+			buf = buf >> 1
+			bit := uint8(uart.Rx.Read())
+			fmt.Print(bit) //fmt包下的Println()方法
+			//fmt.Println(count, t, bit)
+			//buf = buf | (bit << 7)
+			if bit == 0x01 {
+				buf = buf | 0x80000000
+			}
+			count++
+		}
+		if count > 38 {
+			break
+		}
+
+	}
+	return
+
+}
 
 func (uart Uart) Read() (buf uint8) {
 	uart.Rx.Input()
@@ -23,12 +61,12 @@ func (uart Uart) Read() (buf uint8) {
 			break
 		}
 	}
-	for t := range ticker.C {
+	for range ticker.C {
 
-		if count < 8  {
+		if count < 8 {
 			buf = buf >> 1
 			bit := uint8(uart.Rx.Read())
-			fmt.Println(count, t, bit)
+			//fmt.Println(count, t, bit)
 			//buf = buf | (bit << 7)
 			if bit == 0x01 {
 				buf = buf | 0x80
@@ -38,5 +76,6 @@ func (uart Uart) Read() (buf uint8) {
 			break
 		}
 	}
+	<-ticker.C
 	return
 }
